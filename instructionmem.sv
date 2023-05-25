d5dd6855b3e9179807246ca7214680c220deeb53
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2023-05-25 14:41:49 -0700

    Created instructinmem but not sure

---
+++ instructionmem.sv
@@ -0,0 +1,19 @@
+module instructionmem #(parameter PC_BITS = 12)(
+    input [PC_BITS - 1:0] pc,
+    output [2:0] instructions, reg1, reg2
+);
+
+    logic[8:0] core[2**D];
+    logic[8:0] ins;
+    initial begin
+        $readmemb("machinecode.txt",core);
+    end
+
+    always_comb begin
+        ins = core[pc];
+        instructions = ins[8:6];
+        reg1 = ins[5:3];
+        reg2 = ins[2:0];
+    end
+    
+endmodule
