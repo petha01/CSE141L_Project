1243300273e44e9ea24e4274671521a431a0d21f
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2023-05-25 14:12:12 -0700

    Fixed alu

--- alu.sv
+++ alu.sv
@@ -16,15 +16,15 @@ module alu(
             XOR : outVal = input1 ^ input2;
             AND : outVal = input1 & input2;
             RSL : begin
-							outVal[0] = input1[(0 + 7 - input2) % 7];
-							outVal[1] = input1[(1 + 7 - input2) % 7];
-							outVal[2] = input1[(2 + 7 - input2) % 7];
-							outVal[3] = input1[(3 + 7 - input2) % 7];
-							outVal[4] = input1[(4 + 7 - input2) % 7];
-							outVal[5] = input1[(5 + 7 - input2) % 7];
-							outVal[6] = input1[(6 + 7 - input2) % 7];
-							outVal[7] = input1[(7 + 7 - input2) % 7];
-						end
+				outVal[0] = input1[(0 + 7 - input2) % 7];
+				outVal[1] = input1[(1 + 7 - input2) % 7];
+				outVal[2] = input1[(2 + 7 - input2) % 7];
+				outVal[3] = input1[(3 + 7 - input2) % 7];
+				outVal[4] = input1[(4 + 7 - input2) % 7];
+				outVal[5] = input1[(5 + 7 - input2) % 7];
+				outVal[6] = input1[(6 + 7 - input2) % 7];
+				outVal[7] = input1[(7 + 7 - input2) % 7];
+			end
             MOV : outVal = input2;
             LD  : outVal = input1;
             ST  : outVal = input1;
@@ -35,7 +35,7 @@ module alu(
 
     always_ff @ (posedge clock) begin
         out <= outVal;
-        if ((aluOp == BLQZ) && (input1 == input2)) begin
+        if ((aluOp == BLQZ) && (input1 <= input2)) begin
             jumpFlag = 1;
         end else begin
             jumpFlag = 0;
