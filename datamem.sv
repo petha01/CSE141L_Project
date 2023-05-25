600d020863747e299a95efa4f7aa5666a7f5b94c
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2023-05-24 22:41:27 -0700

    Added datamem but not sure if compiles

---
+++ datamem.sv
@@ -0,0 +1,20 @@
+module datamem(
+    input clock, memWrite,
+    input [7:0] addr, data_in,
+    output [7:0] data_out
+);
+
+    logic[7:0] core[256];
+
+    initial begin
+        $readmemh("filename.list", core);
+    end
+
+    assign data_out = core[addr];
+
+    always_ff @ (posedge clock) begin
+        if (memWrite) begin
+            core[addr] <= data_in;
+        end
+    end
+endmodule
