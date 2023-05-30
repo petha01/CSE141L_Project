9fce076afd0f3a9b5eeeb7a11ba00a758038af38
karthikv6 <44593407+karthikv6@users.noreply.github.com>
2023-05-29 22:10:06 -0700

    top_level_instantiation update

--- top_level_instantiation.sv
+++ top_level_instantiation.sv
@@ -1,26 +1,38 @@
 import definitions::*;
-module top_level_instantiation(
+module top_level(
     input clock,
-    input reset
+    input reset,
+    input start
 );
 
-    parameter PC_BITS = 9;
+    parameter PC_BITS = 12;
 
     logic [2:0] aluOp, reg1, reg2;
-    logic [7:0] data1, data2, aluResult, memData, ltVal;
-    logic memWrite, jumpFlag, regWrite, immediate;
-    logic [PC_BITS-1:0] pc;
+    logic [7:0] input1, input2, aluOut, memData, ltVal, target;
+    logic memWrite, jumpFlag, regWrite, immediate, nextIns;
+    logic [PC_BITS-1:0] pc_in, pc_out, startingAddress;
 
-    instructionmem #(PC_BITS) IM (.pc(pc), .instructions(aluOp), .reg1(reg1), .reg2(reg2));
-    programcounter PC (.clock(clock), .reset(reset), .jumpFlag(jumpFlag), .pc(pc));
+    instructionmem #(PC_BITS) IM (.pc(pc_out), .instructions(aluOp), .reg1(reg1), .reg2(reg2));
+    programcounter PC (.nextIns(nextIns), .jumpFlag(jumpFlag), .start(start), .clock(clock),
+                       .pc_in(pc_in), .startingAddress(startingAddress), .target(target), .pc_out(pc_out));
     registerFile RF (.clock(clock), .immediate(immediate), .regWrite(regWrite),
-                     .readRegister1(reg1), .readRegister2(reg2), .writeRegister(reg1),
-                     .ltValue(ltVal), .writeData(aluResult), 
-                     .readData1(data1), .readData2(data2));
-    alu ALU (.aluOp(aluOp), .input1(data1), .input2(data2), 
-             .jumpFlag(jumpFlag), .out(aluResult));
-    datamem DM (.clock(clock), .memWrite(memWrite), .addr(reg1), .data_in(aluResult), .data_out(memData));
+                     .readRegister1(reg1), .readRegister2(reg2),
+                     .ltValue(ltVal), .writeData(aluOut), 
+                     .readData1(input1), .readData2(input2));
+    alu ALU (.aluOp(aluOp), .input1(input1), .input2(input2), 
+             .jumpFlag(jumpFlag), .out(aluOut));
+    datamem DM (.clock(clock), .memWrite(memWrite), .addr(reg1), .data_in(aluOut), .data_out(memData));
 
-    control CONTROL (.clock(clock), .reset(reset), .instructions(aluOp), .jumpFlag(jumpFlag), .memWrite(memWrite), .regWrite(regWrite), .immediate(immediate), .ltVal(ltVal));
+    control CD (.clock(clock), .instructions(pc_out), .aluOp(aluOp), .nextIns(nextIns),
+                .immediate(immediate), .regWrite(regWrite), .memWrite(memWrite), .memToReg(memToReg));
+
+
+    always_ff @(posedge clock) begin
+        if (start || (nextIns && !jumpFlag)) begin
+            nextIns <= 1'b1;
+        end else begin
+            nextIns <= 1'b0;
+        end
+    end
 
 endmodule
