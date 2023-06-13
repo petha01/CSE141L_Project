30b4399ce01901b19dc6c64bb481b25cdfc20c4e
Karisma Kumar <kkarisma12@gmail.com>
2023-06-12 23:21:39 -0700

    Update assemble.py
    Added branching and calculating any number using the LU table

--- assemble.py
+++ assemble.py
@@ -25,60 +25,98 @@ registers = {
 }
 
 lookup_table = {
-  '#0': '000',
-  '#1': '001',
-  '#30': '010',
-  '#-30': '011',
-  # Add more integers if needed
+  '100': '000',
+  '10': '010',
+  '5': '011',
+  '1': '101',
+  '0': '110',
+  '-1': '111',
 }
 
+branches = {}
+
 def translate(assembly_file, machine_file):
+
+  total_lines = 0
   # read the assembly program file
   input = open(assembly_file, 'r')
   file_string = input.read()
   
+  # output the machine code file
   output = open(machine_file, 'w')
   
   # Split the assembly code into individual lines
   lines = file_string.split('\n')
 
   for line in lines:
-    words = line.strip().split()
-    if len(words) == 0 or words[0] == "//":
+    branch = False
+    if line[:2] == "//":
+      continue
+
+    words = line.replace(",", "").split()
+    if len(words) == 0:
+      continue
+    
+    if ':' in line:
+      jump = line.replace(":", "").split()
+      branches[jump[0]] = total_lines + 1
       continue
 
     # Check if instruction is valid
     if words[0] in instructions:
+      if words[0] == 'blqz':
+        branch = True
       opcode = instructions[words[0]]
     else:
-      print("Error in line: ")
-      print(line)
-      raise ValueError(f"Invalid instruction: {words[0]}")
-  
+      raise ValueError(f"Invalid instruction: {words[0]} in line {line}")
+
     # Check if operand/destination is valid
     if words[1] in registers:
       reg1 = registers[words[1]]
     else:
-      print("Error in line: ")
-      print(line)
-      raise ValueError(f"Invalid register: {words[1]}")
+      raise ValueError(f"Invalid register: {words[1]} in line {line}")
     
     # Check if operand register is valid
-    if words[2][0] == '#':
-      reg2 = lookup_table[words[2]]
-    elif words[2] in registers:
+    
+    if branch:   # if it is a branch
+      num = total_lines - branches[words[2]] + 1
+      print(str(num) + "\n")
+      if num in lookup_table:
+        reg2 = lookup_table[words[2]]
+      
+    if words[2] in registers:
       reg2 = registers[words[2]]
+    elif words[2] in lookup_table:
+      reg2 = lookup_table[words[2]]
     else:
-      print("Error in line: ")
-      print(line)
-      raise ValueError(f"Invalid register: {words[1]}")
+      try:
+        if not branch:
+          num = int(words[2])
+        keys_except_last_two = list(lookup_table.keys())[:-2]
+        output.write(instructions['mov'] + registers['r4'] + lookup_table['0'] + "\n")
+        print(instructions['mov'] + registers['r4'] + lookup_table['0'])
+        total_lines += 1
+
+        for key in keys_except_last_two:
+          while num >= int(key):
+            output.write(instructions['mov'] + registers['r5'] + lookup_table[key] + "\n")
+            output.write(instructions['add'] + registers['r4'] + registers['r5'] + "\n")
+            print(instructions['mov'] + registers['r5'] + lookup_table[key])
+            print(instructions['add'] + registers['r4'] + registers['r5'])
+            total_lines += 2
+            num -= int(key)
+        reg2 = registers['r4']
+      except:
+        raise ValueError(f"Invalid register: {words[1]} in line {line}")
     
     machine_line = opcode + reg1 + reg2
+    output.write(machine_line + "\n")
     print(machine_line)
+    total_lines += 1
 
   # Close the file
   input.close()
   output.close()
-translate('test_files/test.txt', 'test_files/output.txt')
+translate('test_files/program1.txt', 'test_files/program1_out.txt')
 
   
