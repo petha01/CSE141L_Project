782f66e3d7aadcf38daa6df5e26d937423459e2c
petha01 <pnataraj@ucsd.edu>
2023-06-15 11:07:24 -0700

    debug statements

--- mux2x1_Nbits.sv
+++ mux2x1_Nbits.sv
@@ -5,10 +5,13 @@ module mux2x1_Nbits #(parameter N = 8) (
     output logic [N-1:0] Y
 );
     always_comb begin
+        $displayb("mux A: %d", A);
+        $displayb("mux B: %d", B);
         if (select == 1'b0)
             Y = A;
         else
             Y = B;
+        $displayb("mux Y: %d", Y);
     end
 endmodule
  
--- registerFile.sv
+++ registerFile.sv
@@ -8,25 +8,26 @@ module registerFile(
     logic [7:0] registers[7:0];
 
     always_ff @(posedge clock) begin
-        $display("Register File readRegister1: %d",readRegister1);
-        $display("Register File readRegister2: %d",readRegister2);
+        // $display("Register File readRegister1: %d",readRegister1);
+        // $display("Register File readRegister2: %d",readRegister2);
 
         if (regWrite) begin
+            $display("Register File writeData: %d",writeData);
             registers[readRegister1] <= writeData;
         end
 
         if (immediate) begin
             readData1 <= registers[readData1];
             readData2 <= ltValue;
-            $display("Register File readData1: %d",readData1);
-            $display("Register File readData2: %d",readData2);
+            // $display("Register File readData1: %d",readData1);
+            // $display("Register File readData2: %d",readData2);
         end
 
         else begin
             readData1 <= registers[readData1];
             readData2 <= registers[readData2];
-            $display("Register File readData1: %d",readData1);
-            $display("Register File readData2: %d",readData2);
+            // $display("Register File readData1: %d",readData1);
+            // $display("Register File readData2: %d",readData2);
         end
     end
 
